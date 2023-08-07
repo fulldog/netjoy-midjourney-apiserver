dc8dda45e9c5c2b9168ac2952bd9421f77239ab7
yongkun.xiong <weilone@vip.qq.com>
2023-08-07 11:53:07 +0800

    v0.02

--- cmd/app/app.go
+++ cmd/app/app.go
@@ -1,14 +1,20 @@
 package app
 
 import (
+	"encoding/json"
 	"github.com/hongliang5316/midjourney-apiserver/internal/application"
 	"github.com/hongliang5316/midjourney-apiserver/internal/config"
-	"log"
 )
 
-func Run(cfg *config.Config) {
+func Run(js []byte) error {
+	var cfg config.Config
+	err := json.Unmarshal(js, &cfg)
+	if err != nil {
+		return err
+	}
 	app := application.NewCfg(cfg)
 	if err := app.Run(); err != nil {
-		log.Fatalf("Call app.Run failed, err: %+v", err)
+		return err
 	}
+	return nil
 }
